5a609e920425a0edc1fe11c102c3657325cca105
NULL511 <null511@outlook.com>
2025-10-21 14:00:23 -0400

    misc fixes

--- shaders/pack.ts
+++ shaders/pack.ts
@@ -1,29 +1,35 @@
+const ENABLE_TAA = false;
+
+
 export function configureRenderer(renderer: RendererConfig): void {
-    renderer.mergedHandDepth = true;
     renderer.ambientOcclusionLevel = 1.0;
-    renderer.disableShade = false;
     renderer.render.entityShadow = true;
+    renderer.mergedHandDepth = true;
+    renderer.disableShade = false;
 }
 
 export function configurePipeline(pipeline: PipelineConfig): void {
-    const texVelocity = pipeline.createTexture("texVelocity")
-        .width(screenWidth)
-        .height(screenHeight)
-        .format(Format.RG16F)
-        .build();
-
     const texFinal = pipeline.createImageTexture("texFinal", "imgFinal")
         .width(screenWidth)
         .height(screenHeight)
         .format(Format.RGBA16F)
         .build();
 
-    pipeline.createImageTexture("texFinalPrev", "imgFinalPrev")
-        .width(screenWidth)
-        .height(screenHeight)
-        .format(Format.RGBA16F)
-        .clear(false)
-        .build();
+    let texVelocity : BuiltTexture | undefined;
+    if (ENABLE_TAA) {
+        pipeline.createImageTexture("texFinalPrev", "imgFinalPrev")
+            .width(screenWidth)
+            .height(screenHeight)
+            .format(Format.RGBA16F)
+            .clear(false)
+            .build();
+
+        texVelocity = pipeline.createTexture("texVelocity")
+            .width(screenWidth)
+            .height(screenHeight)
+            .format(Format.RG16F)
+            .build();
+    }
 
     pipeline.createBuffer("ssboScene", 8, false);
     //const sceneSettings = pipeline.createStreamingBuffer("sceneSettings", 8);
@@ -38,45 +44,49 @@ export function configurePipeline(pipeline: PipelineConfig): void {
     preRender.end();
 
 
-    pipeline.createObjectShader("basic", Usage.BASIC)
-        .location("objects/basic")
-        .exportBool("disableFog", false)
-        .exportBool("EnableTAA", true)
-        .target(0, texFinal)
-        .target(1, texVelocity)
-        .compile();
-
-    pipeline.createObjectShader("sky", Usage.SKY_TEXTURES)
+    const shaderSky = pipeline.createObjectShader("sky", Usage.SKY_TEXTURES)
         .location("objects/basic")
         .exportBool("disableFog", true)
-        .exportBool("EnableTAA", true)
-        .target(0, texFinal)
-        .target(1, texVelocity)
-        .compile();
+        .exportBool("EnableTAA", ENABLE_TAA)
+        .target(0, texFinal);
+    if (ENABLE_TAA) shaderSky.target(1, texVelocity);
+    shaderSky.compile();
 
+    const shaderBasic = pipeline.createObjectShader("basic", Usage.BASIC)
+        .location("objects/basic")
+        .exportBool("disableFog", false)
+        .exportBool("EnableTAA", ENABLE_TAA)
+        .target(0, texFinal);
+    if (ENABLE_TAA) shaderBasic.target(1, texVelocity);
+    shaderBasic.compile();
+    
 
     const postRender = pipeline.forStage(Stage.POST_RENDER);
 
-    postRender.createCompute("taa")
-        .location("post/taa", "applyTaa")
-        .workGroups(
-            Math.ceil(screenWidth / 16),
-            Math.ceil(screenHeight / 16),
-            1)
-        .compile();
+    if (ENABLE_TAA) {
+        postRender.createCompute("taa")
+            .location("post/taa", "applyTaa")
+            .workGroups(
+                Math.ceil(screenWidth / 16),
+                Math.ceil(screenHeight / 8),
+                1)
+            .compile();
+    }
 
     postRender.createComposite("tonemap")
         .location("post/tonemap", "tonemap")
         .target(0, texFinal)
         .compile();
 
-    postRender.createCompute("sharpen")
-        .location("post/sharpen", "sharpen")
-        .workGroups(
-            Math.ceil(screenWidth / 16),
-            Math.ceil(screenHeight / 16),
-            1)
-        .compile();
+    if (ENABLE_TAA) {
+        postRender.createCompute("sharpen")
+            .location("post/sharpen", "sharpen")
+            .workGroups(
+                Math.ceil(screenWidth / 16),
+                Math.ceil(screenHeight / 8),
+                1)
+            .compile();
+    }
 
     postRender.end();
 
